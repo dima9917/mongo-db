b251fb5bbebe2e6b992ce964447476fe287ea3de
dima9917 <dima9917@gmail.com>
2018-08-27 21:05:47 -0700

    S6L32: Removing records

---
+++ project-1/test/delete_test.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Deleting a user', () => {
+    let joe;
+    beforeEach(done=>{
+        joe = new User({name: 'Joe'});
+        joe.save()
+            .then(()=>done());
+    });
+
+    it('Model instance remove', (done) => {
+        joe.remove()
+            .then(() => User.findOne({name: 'Joe'}))
+            .then(user=>{
+                assert(user===null);
+                done();
+            });
+    });
+    
+    it('class method remove', () => {
+
+    });
+
+    it('class method findAndRemove', () => {
+
+    });
+
+    it('class method findByIdAndRemove', () => {
+
+    });
+});
